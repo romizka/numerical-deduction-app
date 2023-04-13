2945d8b0990b1995067a4edc42d3ab1f63711ccf
romizka <awencka1@gmail.com>
2023-04-13 16:22:44 +0200

    create public folder

--- src/main.js
+++ src/main.js
@@ -1,85 +1,114 @@
+import { tasksList } from "./tasksList.js";
 
-import { tasksList } from './tasksList.js';
+const introSection = document.querySelector(".introduction");
+const tasksSection = document.querySelector(".tasks");
+const scoreSection = document.querySelector(".score");
+const startBtn = document.getElementById("startButton");
+const submitBtn = document.getElementById("submitButton");
+const tasksElements = document.querySelector(".tasks_elements");
+const scoreElement = document.querySelector(".score__element");
 
-      const introSection = document.querySelector('.introduction');
-      const tasksSection = document.querySelector('.tasks');
-      const scoreSection = document.querySelector('.score');
-      const startBtn = document.getElementById('startButton');
-      const submitBtn = document.getElementById('submitButton');
-      const tasksElements = document.querySelector('.tasks_elements');
-      const scoreElement = document.querySelector('.score__element')
+tasksElements.insertAdjacentHTML(
+  "beforeEnd",
+  tasksList
+    .map(
+      (item) => `
 
-tasksElements.insertAdjacentHTML('beforeEnd', tasksList.map(item => `
-
-<h3 class="mb-2 mt-8 font-semibold text-blue-600 font-bold text-center uppercase">Task ${tasksList.indexOf(item) + 1}</h3>
-<h4 class="mb-2 font-semibold text-gray-600 text-center tracking-widest">${item.question}</h4>
+<h3 class="mb-2 mt-8 font-semibold text-blue-600 font-bold text-center uppercase">Task ${
+        tasksList.indexOf(item) + 1
+      }</h3>
+<h4 class="mb-2 font-semibold text-gray-600 text-center tracking-widest">${
+        item.question
+      }</h4>
 <ul class="items-center w-full text-sm font-medium text-gray-400 bg-white border border-gray-200 rounded-lg sm:flex">
 <li class="w-full border-b border-gray-200 sm:border-b-0 sm:border-r">
   <div class="flex items-center pl-3 bg-blue-100">
-      <input id="horizontal-list-radio-license" type="radio" value="${item.answers[0].value}" name='task-${tasksList.indexOf(item)}' class="w-4 h-4 text-blue-600 bg-white border-gray-300 focus:ring-blue-500 focus:ring-2">
-      <label for="horizontal-list-radio-license" class="w-full py-3 ml-2 text-sm font-medium text-gray-600 text-left">A: ${item.answers[0].answer}</label>
+      <input id="horizontal-list-radio-license" type="radio" value="${
+        item.answers[0].value
+      }" name='task-${tasksList.indexOf(
+        item
+      )}' class="w-4 h-4 text-blue-600 bg-white border-gray-300 focus:ring-blue-500 focus:ring-2">
+      <label for="horizontal-list-radio-license" class="w-full py-3 ml-2 text-sm font-medium text-gray-600 text-left">A: ${
+        item.answers[0].answer
+      }</label>
   </div>
 </li>
 <li class="w-full border-b border-gray-200 sm:border-b-0 sm:border-r">
   <div class="flex items-center pl-3 bg-blue-100">
-      <input id="horizontal-list-radio-id" type="radio" value="${item.answers[1].value}" name='task-${tasksList.indexOf(item)}' class="w-4 h-4 text-blue-600 bg-white border-gray-300 focus:ring-blue-500 focus:ring-2">
-      <label for="horizontal-list-radio-id" class="w-full py-3 ml-2 text-sm font-medium text-gray-600 text-left">B: ${item.answers[1].answer}</label>
+      <input id="horizontal-list-radio-id" type="radio" value="${
+        item.answers[1].value
+      }" name='task-${tasksList.indexOf(
+        item
+      )}' class="w-4 h-4 text-blue-600 bg-white border-gray-300 focus:ring-blue-500 focus:ring-2">
+      <label for="horizontal-list-radio-id" class="w-full py-3 ml-2 text-sm font-medium text-gray-600 text-left">B: ${
+        item.answers[1].answer
+      }</label>
   </div>
 </li>
 <li class="w-full border-b border-gray-200 sm:border-b-0 sm:border-r">
   <div class="flex items-center pl-3 bg-blue-100">
-      <input id="horizontal-list-radio-millitary" type="radio" value="${item.answers[2].value}" name='task-${tasksList.indexOf(item)}' class="w-4 h-4 text-blue-600 bg-white border-gray-300 focus:ring-blue-500 focus:ring-2">
-      <label for="horizontal-list-radio-millitary" class="w-full py-3 ml-2 text-sm font-medium text-gray-600 text-left">C: ${item.answers[2].answer}</label>
+      <input id="horizontal-list-radio-millitary" type="radio" value="${
+        item.answers[2].value
+      }" name='task-${tasksList.indexOf(
+        item
+      )}' class="w-4 h-4 text-blue-600 bg-white border-gray-300 focus:ring-blue-500 focus:ring-2">
+      <label for="horizontal-list-radio-millitary" class="w-full py-3 ml-2 text-sm font-medium text-gray-600 text-left">C: ${
+        item.answers[2].answer
+      }</label>
   </div>
 </li>
 <li class="w-full">
   <div class="flex items-center pl-3 bg-blue-100">
-      <input id="horizontal-list-radio-passport" type="radio" value="${item.answers[3].value}" name='task-${tasksList.indexOf(item)}' class="w-4 h-4 text-blue-600 bg-white border-gray-300 focus:ring-blue-500 focus:ring-2">
-      <label for="horizontal-list-radio-passport" class="w-full py-3 ml-2 text-sm font-medium text-gray-600 text-left">D: ${item.answers[3].answer}</label>
+      <input id="horizontal-list-radio-passport" type="radio" value="${
+        item.answers[3].value
+      }" name='task-${tasksList.indexOf(
+        item
+      )}' class="w-4 h-4 text-blue-600 bg-white border-gray-300 focus:ring-blue-500 focus:ring-2">
+      <label for="horizontal-list-radio-passport" class="w-full py-3 ml-2 text-sm font-medium text-gray-600 text-left">D: ${
+        item.answers[3].answer
+      }</label>
   </div>
 </li>
 </ul>
 
-`).join(''));
+`
+    )
+    .join("")
+);
 
-    
-      const startQuiz = function() {
-          tasksSection.classList.toggle('hide');
-          introSection.classList.toggle('hide');
-          submitBtn.classList.toggle('hide');
-        }
+const startQuiz = function () {
+  tasksSection.classList.toggle("hide");
+  introSection.classList.toggle("hide");
+  submitBtn.classList.toggle("hide");
+};
 
+let totalScore = 0;
 
-      let totalScore = 0;
+function addPoints() {
+  const element = document.getElementsByTagName("input");
 
-      
-      function addPoints() {
-      const element = document.getElementsByTagName('input');
-      
-      for(let i = 0; i < element.length; i++) {
-        console.log(element[i].value);
-        if (element[i].type === "radio" && element[i].checked) {
-          if (element[i].value == 1) {
-            totalScore += 1;
-           }
-          }
-        }
-        }
+  for (let i = 0; i < element.length; i++) {
+    console.log(element[i].value);
+    if (element[i].type === "radio" && element[i].checked) {
+      if (element[i].value == 1) {
+        totalScore += 1;
+      }
+    }
+  }
+}
 
+const showResult = function () {
+  scoreElement.innerHTML = `Your total score: <br> ${totalScore} / 21`;
 
-    const showResult = function() {
-      scoreElement.innerHTML = `Your total score: <br> ${totalScore} / 21`;
+  tasksSection.classList.toggle("hide");
+  scoreSection.classList.toggle("hide");
+  submitBtn.classList.toggle("hide");
+};
 
-      tasksSection.classList.toggle('hide');
-      scoreSection.classList.toggle('hide');
-      submitBtn.classList.toggle('hide');
-    
-        }
+const onSubmit = () => {
+  addPoints(), showResult();
+};
 
-        const onSubmit = () => {addPoints(), showResult()};
+startBtn.addEventListener("click", startQuiz);
 
-
-
-        startBtn.addEventListener('click', startQuiz);
-
-        submitBtn.addEventListener('click', onSubmit);
+submitBtn.addEventListener("click", onSubmit);
--- src/tasksList.js
+++ src/tasksList.js
@@ -1,214 +1,211 @@
-
 export const tasksList = [
-    {
-        question: '0 5 10 15 20 ?', 
-        answers: [
-            {answer: '20', value: 0},
-            {answer: '25', value: 1},
-            {answer: '30', value: 0},
-            {answer: '21', value: 0},
-        ]
-    },
+  {
+    question: "0 5 10 15 20 ?",
+    answers: [
+      { answer: "20", value: 0 },
+      { answer: "25", value: 1 },
+      { answer: "30", value: 0 },
+      { answer: "21", value: 0 },
+    ],
+  },
 
-    {
-        question: '.25 .5 1 2 4 ?', 
-        answers: [
-            {answer: '12', value: 0},
-            {answer: '16', value: 0},
-            {answer: '8', value: 1},
-            {answer: '10', value: 0},
-        ]
-    },
+  {
+    question: ".25 .5 1 2 4 ?",
+    answers: [
+      { answer: "12", value: 0 },
+      { answer: "16", value: 0 },
+      { answer: "8", value: 1 },
+      { answer: "10", value: 0 },
+    ],
+  },
 
-    {
-        question: '98 50 26 14 8 ?', 
-        answers: [
-            {answer: '4', value: 0},
-            {answer: '2', value: 0},
-            {answer: '6', value: 0},
-            {answer: '5', value: 1},
-        ]
-    },
+  {
+    question: "98 50 26 14 8 ?",
+    answers: [
+      { answer: "4", value: 0 },
+      { answer: "2", value: 0 },
+      { answer: "6", value: 0 },
+      { answer: "5", value: 1 },
+    ],
+  },
 
-    {
-        question: '1 2 3 5 8 ?', 
-        answers: [
-            {answer: '5', value: 0},
-            {answer: '11', value: 0},
-            {answer: '8', value: 0},
-            {answer: '13', value: 1},
-        ]
-    },
+  {
+    question: "1 2 3 5 8 ?",
+    answers: [
+      { answer: "5", value: 0 },
+      { answer: "11", value: 0 },
+      { answer: "8", value: 0 },
+      { answer: "13", value: 1 },
+    ],
+  },
 
-    {
-        question: '4 8 12 16 20 ?', 
-        answers: [
-            {answer: '25', value: 0},
-            {answer: '22', value: 0},
-            {answer: '24', value: 1},
-            {answer: '28', value: 0},
-        ]
-    },
+  {
+    question: "4 8 12 16 20 ?",
+    answers: [
+      { answer: "25", value: 0 },
+      { answer: "22", value: 0 },
+      { answer: "24", value: 1 },
+      { answer: "28", value: 0 },
+    ],
+  },
 
-    {
-        question: '160 120 100 90 85 ?', 
-        answers: [
-            {answer: '78.5', value: 0},
-            {answer: '80', value: 0},
-            {answer: '82.5', value: 0},
-            {answer: '84', value: 1},
-        ]
-    },
+  {
+    question: "160 120 100 90 85 ?",
+    answers: [
+      { answer: "78.5", value: 0 },
+      { answer: "80", value: 0 },
+      { answer: "82.5", value: 0 },
+      { answer: "84", value: 1 },
+    ],
+  },
 
-    {
-        question: '.55 .65 .75 .85 .95 ?', 
-        answers: [
-            {answer: '1.05', value: 0},
-            {answer: '1.5', value: 0},
-            {answer: '1.15', value: 0},
-            {answer: '9.5', value: 1},
-        ]
-    },
+  {
+    question: ".55 .65 .75 .85 .95 ?",
+    answers: [
+      { answer: "1.05", value: 0 },
+      { answer: "1.5", value: 0 },
+      { answer: "1.15", value: 0 },
+      { answer: "9.5", value: 1 },
+    ],
+  },
 
-    {
-        question: '1 3 8 19 42 ?', 
-        answers: [
-            {answer: '84', value: 0},
-            {answer: '89', value: 0},
-            {answer: '71', value: 0},
-            {answer: '85', value: 1},
-        ]
-    },
-    
-    {
-        question: '2 7 12 17 22 ?', 
-        answers: [
-            {answer: '26', value: 0},
-            {answer: '28', value: 0},
-            {answer: '23', value: 0},
-            {answer: '27', value: 1},
-        ]
-    },
+  {
+    question: "1 3 8 19 42 ?",
+    answers: [
+      { answer: "84", value: 0 },
+      { answer: "89", value: 0 },
+      { answer: "71", value: 0 },
+      { answer: "85", value: 1 },
+    ],
+  },
 
-    
-    {
-        question: '1 7 13 19 25 ?', 
-        answers: [
-            {answer: '18', value: 0},
-            {answer: '15', value: 0},
-            {answer: '31', value: 0},
-            {answer: '33', value: 1},
-        ]
-    },
+  {
+    question: "2 7 12 17 22 ?",
+    answers: [
+      { answer: "26", value: 0 },
+      { answer: "28", value: 0 },
+      { answer: "23", value: 0 },
+      { answer: "27", value: 1 },
+    ],
+  },
 
-    {
-        question: '3 8 22 63 185 ?', 
-        answers: [
-            {answer: '550', value: 1},
-            {answer: '270', value: 0},
-            {answer: '365', value: 0},
-            {answer: '248', value: 0},
-        ]
-    },
+  {
+    question: "1 7 13 19 25 ?",
+    answers: [
+      { answer: "18", value: 0 },
+      { answer: "15", value: 0 },
+      { answer: "31", value: 0 },
+      { answer: "33", value: 1 },
+    ],
+  },
 
-    {
-        question: '7 7 9 13 19 ?', 
-        answers: [
-            {answer: '25', value: 0},
-            {answer: '29', value: 0},
-            {answer: '31', value: 0},
-            {answer: '27', value: 1},
-        ]
-    },
+  {
+    question: "3 8 22 63 185 ?",
+    answers: [
+      { answer: "550", value: 1 },
+      { answer: "270", value: 0 },
+      { answer: "365", value: 0 },
+      { answer: "248", value: 0 },
+    ],
+  },
 
-    {
-        question: '1 1 2 4 7 ?', 
-        answers: [
-            {answer: '6', value: 0},
-            {answer: '11', value: 1},
-            {answer: '8', value: 0},
-            {answer: '12', value: 0},
-        ]
-    },
+  {
+    question: "7 7 9 13 19 ?",
+    answers: [
+      { answer: "25", value: 0 },
+      { answer: "29", value: 0 },
+      { answer: "31", value: 0 },
+      { answer: "27", value: 1 },
+    ],
+  },
 
-    {
-        question: '0 –1 0 3 8 ?', 
-        answers: [
-            {answer: '15', value: 1},
-            {answer: '11', value: 0},
-            {answer: '12', value: 0},
-            {answer: '24', value: 0},
-        ]
-    },
+  {
+    question: "1 1 2 4 7 ?",
+    answers: [
+      { answer: "6", value: 0 },
+      { answer: "11", value: 1 },
+      { answer: "8", value: 0 },
+      { answer: "12", value: 0 },
+    ],
+  },
 
-    {
-        question: '0 3 3 6 9 ?', 
-        answers: [
-            {answer: '12', value: 0},
-            {answer: '15', value: 1},
-            {answer: '18', value: 0},
-            {answer: '9', value: 0},
-        ]
-    },
+  {
+    question: "0 –1 0 3 8 ?",
+    answers: [
+      { answer: "15", value: 1 },
+      { answer: "11", value: 0 },
+      { answer: "12", value: 0 },
+      { answer: "24", value: 0 },
+    ],
+  },
 
-    {
-        question: '6 9 3 8 3 ?', 
-        answers: [
-            {answer: '7', value: 0},
-            {answer: '6', value: 0},
-            {answer: '8', value: 0},
-            {answer: '10', value: 1},
-        ]
-    },
+  {
+    question: "0 3 3 6 9 ?",
+    answers: [
+      { answer: "12", value: 0 },
+      { answer: "15", value: 1 },
+      { answer: "18", value: 0 },
+      { answer: "9", value: 0 },
+    ],
+  },
 
-    {
-        question: '7 12 9 19 13 ?', 
-        answers: [
-            {answer: '22', value: 0},
-            {answer: '30', value: 0},
-            {answer: '32', value: 0},
-            {answer: '28', value: 1},
-        ]
-    },
+  {
+    question: "6 9 3 8 3 ?",
+    answers: [
+      { answer: "7", value: 0 },
+      { answer: "6", value: 0 },
+      { answer: "8", value: 0 },
+      { answer: "10", value: 1 },
+    ],
+  },
 
-    {
-        question: '75 50 90 65 105 ?', 
-        answers: [
-            {answer: '185', value: 0},
-            {answer: '130', value: 0},
-            {answer: '80', value: 1},
-            {answer: '170', value: 0},
-        ]
-    },
+  {
+    question: "7 12 9 19 13 ?",
+    answers: [
+      { answer: "22", value: 0 },
+      { answer: "30", value: 0 },
+      { answer: "32", value: 0 },
+      { answer: "28", value: 1 },
+    ],
+  },
 
-    {
-        question: '3 9 4 16 11 ?', 
-        answers: [
-            {answer: '27', value: 0},
-            {answer: '44', value: 0},
-            {answer: '25', value: 0},
-            {answer: '35', value: 1},
-        ]
-    },
+  {
+    question: "75 50 90 65 105 ?",
+    answers: [
+      { answer: "185", value: 0 },
+      { answer: "130", value: 0 },
+      { answer: "80", value: 1 },
+      { answer: "170", value: 0 },
+    ],
+  },
 
-    {
-        question: '17 11 28 39 67 ?', 
-        answers: [
-            {answer: '96', value: 0},
-            {answer: '106', value: 1},
-            {answer: '95', value: 0},
-            {answer: '58', value: 0},
-        ]
-    },
+  {
+    question: "3 9 4 16 11 ?",
+    answers: [
+      { answer: "27", value: 0 },
+      { answer: "44", value: 0 },
+      { answer: "25", value: 0 },
+      { answer: "35", value: 1 },
+    ],
+  },
 
-    {
-        question: '5 3 4 9 23 ?', 
-        answers: [
-            {answer: '41', value: 0},
-            {answer: '60', value: 1},
-            {answer: '34', value: 0},
-            {answer: '32', value: 0},
-        ]
-    },
-    
-]
+  {
+    question: "17 11 28 39 67 ?",
+    answers: [
+      { answer: "96", value: 0 },
+      { answer: "106", value: 1 },
+      { answer: "95", value: 0 },
+      { answer: "58", value: 0 },
+    ],
+  },
+
+  {
+    question: "5 3 4 9 23 ?",
+    answers: [
+      { answer: "41", value: 0 },
+      { answer: "60", value: 1 },
+      { answer: "34", value: 0 },
+      { answer: "32", value: 0 },
+    ],
+  },
+];
